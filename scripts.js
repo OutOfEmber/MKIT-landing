3e2e34363c0933c382d11db3f9acdafe6dfd986e
Терещенко Вадим Александрович <v.tereshenko@mkit.local>
2025-10-23 13:59:48 +0300

    js osnovi

---
+++ scripts.js
@@ -0,0 +1,127 @@
+let a=4,b=16;
+(b/a)%2==0 ? console.log("Делится") : console.log("Не делится");
+/*23.10.2025
+
+let a="aasdb"
+a.length>1 ? console.log(a[a.length-2]) : console.log(a[0]);
+
+let white = 'Белый'
+white === 'Белый' ? 'Белый' : 'Не белый'
+if(white === 'Белый'){
+    'Белый'
+}else{
+    'Не белый'
+}
+
+
+let a=1234;
+let b=String (a);
+let c=33542;
+let d=String (c);
+if(b[0]==d[0]){
+    console.log("Первые цифры равны");   
+}
+else{
+    console.log("Первые цифры не равны");
+}
+
+
+
+let a=1234;
+let b=String (a);
+console.log(b[0]);
+console.log(b[b.length-1]);
+let c=Number (b[0]);
+let d=Number (b[b.length-1])
+console.log(c-d);
+
+
+let a="лось";
+if(a[a.length-1]=='ь'){
+    console.log(a[a.length-2]);
+}
+else{
+    console.log(a[a.length-1]);
+}
+
+let a="loh" , b="loser";
+if (a[0]==b[0]) {
+    console.log('Первые буквы совпадают');
+}
+else{
+    console.log('Первые буквы не совпадают');
+}
+
+let x=prompt('Введите число');
+    function Ischet(x) {
+        if (x%2==0) {
+            console.log("Ваше число четное");
+        }
+        else{
+            console.log("Ваше число нечетное");
+            
+        }
+    }
+    Ischet(x); 
+ let a='SuperDuperMEgaAlfaGammaSigmaGODID'
+console.log(a[a.length-1])
+
+let x=prompt('Введите число');
+    function IsNegative(x) {
+        if (x<0) {
+            console.log("Ваше число отрицательное");
+        }
+        else{
+            console.log("Ваше число положительное");
+            
+        }
+    }
+    IsNegative(x);
+*/
+    /*22.10.2025
+    
+
+
+    alert('HELLo World')
+
+
+    let a=prompt('Введите число а');
+    let b=prompt('Введите число b');
+    Number(a);
+    Number(b);
+    console.log(Number(a)+Number(b));
+
+
+    const array=[1,2,3,4,5,6];
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+
+
+    let a = 10,b = 10;
+
+    let key ='*';
+    switch (key) {
+        case '+':
+            console.log(a+b);
+            
+            break;
+        case '-':
+            console.log(a-b);
+            
+            break;
+        case '*':
+            console.log(a*b);
+            
+            break;
+        case '/':
+            console.log(a/b);
+            
+            break;
+    
+        default:
+            console.log('error');
+            
+            break;
+    }
+    */
